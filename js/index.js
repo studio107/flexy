10ef7573f04487f1b916caa21b384da79808f533
Falaleev Maxim <max@studio107.ru>
2017-06-15 17:47:09 +0300

    Fix #12, added em-calc, rem-calc functions, increase grid-gutter to 1em

--- js/index.js
+++ js/index.js
@@ -1,26 +1,4 @@
 import $ from 'jquery';
-import 'magnific-popup';
-import _ from 'underscore';
-
-$(document)
-    .on('click', '[data-modal]', e => {
-        e.preventDefault();
-
-        let $target = $(e.target),
-            html = $($target.attr('href')).wrap('<div/>').parent().html();
-
-        $('.b-source__content').html(_.escape(html));
-
-        $.magnificPopup.open({
-            items: {
-                src: $('.b-source'),
-                type: 'inline'
-            },
-            delegate: 'a',
-            removalDelay: 0,
-            midClick: false
-        }, 0);
-    });
 
 $(document)
     .on('click', '.b-tab__tab', e => {
