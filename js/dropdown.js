fde3d46f36a7e60fc7d83a7d78fcbae98e61b710
Falaleev Maxim <max@studio107.ru>
2018-02-17 23:40:11 +0300

    added tab js component, move breadcrumb settings to _breadcrumb, added flatui color scheme, added vertical rhythm based on shevy

---
+++ js/dropdown.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+
+$(document)
+    .on('click', '.b-dropdown__toggle', e => {
+        let $target = $(e.target),
+            $menu = $target.closest('.b-dropdown').children('.b-dropdown__menu'),
+            openCls = 'b-dropdown__menu_open';
+
+        if ($menu.hasClass(openCls)) {
+            $menu.removeClass(openCls);
+        } else {
+            $('.b-dropdown__menu').removeClass('b-dropdown__menu_open');
+            $menu.addClass(openCls);
+        }
+    })
+    .on('click', e => {
+        let $target = $(e.target);
+        if ($target.closest('.b-dropdown').length > 0) {
+            return e;
+        }
+
+        $('.b-dropdown__menu').removeClass('b-dropdown__menu_open');
+    });
---
+++ js/tab.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+
+$(document)
+    .on('click', '.b-tab__tab', e => {
+        let $target = $(e.target),
+            $container = $target.closest('.b-tab'),
+            $tabs = $container.find('.b-tab__tab'),
+            $content = $container.find('.b-tab__content'),
+            $currentTab = $target.closest('.b-tab__tab'),
+            id = $currentTab.attr('data-tab');
+
+        if ($currentTab.hasClass('b-tab__tab_disabled')) {
+            return;
+        }
+
+        $tabs.removeClass('b-tab__tab_active');
+        $content.removeClass('b-tab__content_active');
+        $container
+            .find('.b-tab__content[data-tab="' + id + '"]')
+            .addClass('b-tab__content_active');
+
+        $currentTab.addClass('b-tab__tab_active');
+    });
